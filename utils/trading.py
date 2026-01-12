8b1ebe271cdf43917b15a93a6f0ae4d3572564dc
trisha <trisha.darure@gmail.com>
2026-01-11 21:06:05 -0600

    created trading simulation logic

---
+++ utils/trading.py
@@ -0,0 +1,11 @@
+def simple_trade_signal(sentiment_score, threshold=0.1):
+    """
+    Determine trade action based on aggregated daily sentiment.
+    threshold: minimum sentiment magnitude to trigger buy/sell
+    """
+    if sentiment_score > threshold:
+        return "BUY"
+    elif sentiment_score < -threshold:
+        return "SELL"
+    else:
+        return "HOLD"
