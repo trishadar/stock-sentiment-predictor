e88bf24766e736b205e05424340123e99d70b6cf
trisha <trisha.darure@gmail.com>
2026-01-13 14:53:43 -0600

    Update stock_fetcher.py

--- utils/stock_fetcher.py
+++ utils/stock_fetcher.py
@@ -7,8 +7,8 @@ def get_stock_price(ticker: str):
 
         if hist.empty:
             return None
-
-        return round(float(hist["Close"].iloc[-1]), 2)
+        price = hist["Close"].iloc[-1]
+        return round(float(price), 2)
 
     except Exception as e:
         print("Stock price error:", e)
